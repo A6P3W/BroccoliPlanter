2e0f1f4d878937274cac709c2f731c597864199b
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-07-13 01:32:20 +0900

    [update] Broccoliの変更に対応。

--- Game/Source/GameMain.cpp
+++ Game/Source/GameMain.cpp
@@ -1,12 +1,21 @@
 #include <DxLib.h>
+#include <Windows.h>
 
-#include "NetworkTypes.h"
 #include "SceneManager.h"
-
+#include "Application.h"
 void SetupGame() {
   auto& SM = SceneManager::GetInstance();
-  // SM.RegisterLevelPath(, ".BLevel");
-
-  // SM.OpenSceneById();
+  SM.SetStartupLevelPath(".BLevel");
   SetMouseDispFlag(1);
+}
+
+int WINAPI
+WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, LPSTR CommandLine, int ShowCommand) {
+  (void)Instance;
+  (void)PreviousInstance;
+  (void)CommandLine;
+  (void)ShowCommand;
+  Application::SetGameSetupCallback(&SetupGame);
+  Application App;
+  return App.Run() ? 0 : 1;
 }
